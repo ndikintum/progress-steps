baf1accbcc740c529e49c3630c62185361111134
ndikintum <ndikintumrodrique2@gmail.com>
2024-01-08 01:51:57 +0100

    progress

--- assets/main.js
+++ assets/main.js
@@ -5,20 +5,20 @@ const circles = document.querySelectorAll('.circle')
 
 let currentActive = 1
 
-next.addEventListener('click', ()=> {
+next.addEventListener('click', () => {
   currentActive++
 
-  if(currentActive > circles.length) {
+  if (currentActive > circles.length) {
     currentActive = circles.length
   }
 
   update()
 })
 
-prev.addEventListener('click', ()=> {
+prev.addEventListener('click', () => {
   currentActive--
 
-  if(currentActive < 1) {
+  if (currentActive < 1) {
     currentActive = 1
   }
 
@@ -27,7 +27,7 @@ prev.addEventListener('click', ()=> {
 
 function update() {
   circles.forEach((circle, index) => {
-    if(index < currentActive) {
+    if (index < currentActive) {
       circle.classList.add('active')
     } else {
       circle.classList.remove('active')
@@ -40,9 +40,9 @@ function update() {
 
   if (currentActive === 1) {
     prev.disabled = true
-  } else if(currentActive === circles.length) {
+  } else if (currentActive === circles.length) {
     next.disabled = true
-  } else{
+  } else {
     prev.disabled = false
     next.disabled = false
   }
